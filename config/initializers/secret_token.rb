e7fa1e1d9e8f4e2357f5e4735e0bef4326bb8cce
Cavin <cstone2003@hotmail.com>
2014-05-17 18:23:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Couponhazel::Application.config.secret_token = 'd4f78001a5923e16552fc13c52f11270151bbd895387cf3d45fbc5cba30d2f09259f05f9c9849857e4cdec9b845f1cebcee92d17e4cc01f3530d8d6055e8ac47'
